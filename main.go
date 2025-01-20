399955b624be573284baea6fca8c2072186ee01f
SulaimonShittu <71654123+SulaimonShittu@users.noreply.github.com>
2025-01-20 12:30:58 +0100

    Data Types & Variables, Contd.

--- main.go
+++ main.go
@@ -2,14 +2,52 @@ package main
 
 import "fmt"
 
+var course string = "Computer Science"
+
 func main() {
-	fmt.Println("Hello World")
-	printff()
+	userinput()
+}
+
+func userinput() {
+	var name string
+	var isstudent bool
+	count, err := fmt.Scanf("%s %t", &name, &isstudent)
+	if err == nil && count == 2 && isstudent == true {
+		fmt.Printf("Welcome, student : %s", name)
+	}
+}
+
+func ZeroValues() {
+	var (
+		a int
+		b string
+		c rune
+		d float64
+		e bool
+	)
+	fmt.Printf("The Zero Values of types,\nint(%T) : %d,\nstring(%T) : %s,\nrune(%T) : %s,\nfloat64(%T) : %f,\nbool(%T) : %t", a, a, b, b, c, string(c), d, d, e, e)
+}
+
+func glovar() {
+	fmt.Println(course)
+	course = "CSC"
+	fmt.Println(course)
 }
 
 func DeclaringVariable() {
 	var greeting string = "Hello World"
 	fmt.Println(greeting)
+
+	// Variables of same type dclaration
+	var a, b int = 20, 45
+	// Varaibles of different types declaration
+	fmt.Println("a : ", a, " ", "b ; ", b)
+	var (
+		name string = "Sulaimon"
+		age  int    = 21
+	)
+	fmt.Printf("Your name is : %s, and age : %d", name, age)
+
 }
 
 func printff() {
